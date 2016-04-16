f3408bc04e0d5d20b93d91ec355aaf2141343f97
Christoph Ortner <c.ortner@warwick.ac.uk>
2016-04-16 14:30:45 +0100

    New version of FEM.jl, now using scipy.spatial.Delaunay

--- src/FEM.jl
+++ src/FEM.jl
@@ -3,7 +3,8 @@
 # `module FEM`
 
 Module that collects some basic finite element functionality for
-application with the Cauchy--Born model. 
+application with the Cauchy--Born model. The core is a Julia
+wrapper for Qhull which goes via `PyCall` and `scipy.spatial.Delaunay`.
 
 ## Quick example
 
@@ -15,39 +16,45 @@ plot(tri; xradius=0.02)
 
 ## List of types and methods
 
-Look at the respective documentions for more info.
+Look at the respective docs for more info.
 
 * `Triangulation` : Delauany triangulation from arbitrary set of points
 * `nT, nX` : number of elements / number of nodes
 * `elements` : iterator over elements
 * `P1_element` : some useful generic computations associated with P1-FEM
 * `plot` : display the mesh
+* `locate` : find elements to which some points belong
 
 ## Dependencies:
 
-* Compose for plotting 
-* PyCall for calling Python Triangle
-* [Python Triangle](https://github.com/drufat/triangle)
+* `Compose` for plotting 
+* `PyCall` for calling Python / scipy
+* working Python installation with `scipy`
 
+## TODO
+
+* Constrained Delaunay triangulation
+* maybe admitting different triangulation packages
 """
 module FEM
 
 using PyCall
 using Compose
 
-@pyimport triangle
+@pyimport scipy.spatial as scipy_spatial
 
 export Triangulation
 export nT, nX
 export elements
+export locate
 export P1_element
 export plot
 
 """`type Triangulation`
 
 An elementary triangulation wrapper type, to allow in principle usage of
-different background triangulation packages. The current implementation
-requires [Python Triangle](https://github.com/drufat/triangle)
+different background triangulation packages. The current implementation uses
+`scipy.spatial.Delaunay`, which is a [Qhull](http://www.qhull.org) wrapper.
 
 * `X` : dim x Npoints array containing the vertices
 * `T` : dim+1 x Ntriangles array containing the element information
@@ -57,15 +64,6 @@ requires [Python Triangle](https://github.com/drufat/triangle)
 * To construct a Delaunay triangulation create a 2 x Npoints or a Npoints x 2
 array `X`, where Npoints must be larger than 2, and simply call
 `tri = Triangulation(X)`.
-
-* For now the code does not allow constrained Delaunay. If the domain is
-just barely convex (e.g. a polygon), then this means that thin elements
-will be created at the boundary. To account for that, if a list of 
-boundary node indices is passed as well, then the domain will first be
-artifically convexified before triangulation:
-```
-  tri = Triangulation(X; bdry=list_of_indices)
-```
 
 ## Usage via iterator
 
@@ -81,26 +79,21 @@ end
 type Triangulation
     X::Matrix{Float64}
     T::Matrix{Int}
+    pyo::PyObject
 end
 
 
-function Triangulation(X; bdry=nothing)
-    # convexify if needed
-    if bdry != nothing
-        # do the convexification!
-    else
-        Y = X
-    end
+function Triangulation(X)
     # reshape X
-    if size(Y, 1) == 2
-        Y = Y'
-    elseif size(Y, 2) != 2
+    if size(X, 1) == 2
+        X = X'
+    elseif size(X, 2) != 2
         error("Triangulation : size(X) must be (2, N) or (N, 2)")
     end
-    # call the Shewchuck-code via python
-    tri = triangle.triangulate(Dict("vertices" => Y))
+    # call the Qhull code via PyCall via scipy bindings
+    tri = scipy_spatial.Delaunay(X)
     # return the Julia object
-    return Triangulation(Y', tri["triangles"]' + 1)
+    return Triangulation(X', tri[:simplices]' + 1, tri)
 end
 
 "return number of elements in the triangulation"
@@ -121,9 +114,9 @@ done(tri::Triangulation, state::ElementIterator) = (state.i == nT(tri))
 next(tri::Triangulation, state::ElementIterator) =
     (state.i+1, ElementIterator(tri, state.i+1))
 
+"compute the P1 gradient in the reference triangle"
+ref_grad(t::Vector{Int}, V::Matrix{Float64}) = V[:, t[2:3]] .- V[:, t[1]]
 
-ref_grad(t::Vector{Int}, V::Matrix{Float64}) =
-    V[:, t[2:3]] .- V[:, t[1]]
 
 """compute some information related to P1-FEM:
 
@@ -147,21 +140,17 @@ function P1_element(el::Int, tri::Triangulation, U::Matrix{Float64})
 end
 
 
-
-
-############### PLOTTING ###############
-
-"""compute unique set of edges: for now this is an auxiliary function use
-for plotting, but it could become something more useful for 
 """
-function _edges_(tri::Triangulation)
-    T = tri.T
-    S = [ T[1,:] T[1,:] T[2,:] T[2,:] T[3,:] T[3,:];
-          T[2,:] T[3,:] T[1,:] T[3,:] T[1,:] T[2,:] ]
-    S = sort(S, 1)
-    return unique(S, 2)
-end
+find element indices to which the provided points belong.
 
+## Parameters
+* `P` : 2 x M, list of points that we want to locate
+* `tri` : Triangulation
+"""
+locate(P::Matrix{Float64}, tri::Triangulation) = tri.pyo[:find_simplex](P') + 1
+
+
+# ########################## PLOTTING ############################
 
 
 function plot(tri; width=15cm, height=:auto, xradius=0.25, atcol="tomato", 
@@ -182,23 +171,40 @@ function plot(tri; width=15cm, height=:auto, xradius=0.25, atcol="tomato",
     ub = UnitBox(xLim[1], yLim[1], dat_width, dat_height)
     
     # draw the elements
-    c_ctx = context(units=ub)
-    for n = 1:FEM.nT(tri)
-        p = tri.X[:, tri.T[:, n]]
-        c_ctx = compose(c_ctx, polygon( [tuple(p[:,1]...),
-                                         tuple(p[:,2]...),
-                                         tuple(p[:,3]...)] ))
+    points = Tuple{Float64, Float64}[]
+    for n = 1:nT(tri)
+        p = [tri.X[:, tri.T[:, n]] tri.X[:, tri.T[1,n]]]
+        for m = 1:size(p, 2)
+            push!(points, tuple(p[1, m], p[2,m]))
+        end
+        push!(points, tuple(NaN, NaN))
     end
-    c_ctx = compose(c_ctx, fill(elcol), stroke(linecol), linewidth(lwidth))
+    c_ctx = compose(context(units=ub), polygon(points), 
+                    fill(elcol), stroke(linecol), linewidth(lwidth) )
     
-    # draw the atoms
+    # draw the nodes
     a_ctx = context(units=ub)
-    a_ctx = compose(a_ctx,
-                    circle(tri.X[1,:], tri.X[2,:], xradius), 
-                    fill(atcol), stroke(linecol) )
-    
+    if xradius > 0
+        a_ctx = compose(a_ctx,
+                        circle(tri.X[1,:], tri.X[2,:], [xradius]), 
+                        fill(atcol), stroke(linecol) )
+    end
+   
     return compose(context(), a_ctx, c_ctx)
-end    
+end
+
 
 end
 
+
+
+# """compute unique set of edges: for now this is an auxiliary function use
+# for plotting, but it could become something more useful for 
+# """
+# function _edges_(tri::Triangulation)
+#     T = tri.T
+#     S = [ T[1,:] T[1,:] T[2,:] T[2,:] T[3,:] T[3,:];
+#           T[2,:] T[3,:] T[1,:] T[3,:] T[1,:] T[2,:] ]
+#     S = sort(S, 1)
+#     return unique(S, 2)
+# end
